e488246af32836ad0f307880325a96f16e4d1e5d
JulianB <belmartinojulian@gmail.com>
2025-09-05 12:19:56 -0300

    company

--- api/createLead.js
+++ api/createLead.js
@@ -25,7 +25,7 @@ export default async function handler(req, res) {
   formData.append("first_name", first_name || "");
   formData.append("last_name", last_name);
   formData.append("email", email || "");
- 
+  formData.append("company", company);
 
   try {
     const response = await fetch(url, {
